b0a85ad6131d950f65d9a976ef84e88628101187
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-05 16:17:06 +0300

    styles to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 const Footer = () => {
   return (
-    <div className="flex p-10 max-w-full gap-10 justify-center bg-black text-white ">
+    <div className="fixed  left-0 w-full flex p-10 max-w-full gap-10 justify-center bg-black text-white bottom-0">
       
       <p>www.junevasRentz.com</p>
       <p>copyright &copy;2024.All rights reserved.Powered by junevaRentz</p>
