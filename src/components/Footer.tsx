812c80be117775cadf48e12dbaa92e52bd659bd9
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-06 18:44:19 +0300

    changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@
 
 const Footer = () => {
   return (
-    <div className="fixed  left-0 w-full flex p-10 max-w-full gap-10 justify-center bg-black text-white bottom-0">
+    <div className="text-sm sm:text-lg left-0 w-full flex p-10 max-w-full gap-5 justify-center bg-black text-white bottom-0">
       
-      <p>www.junevasRentz.com</p>
-      <p>copyright &copy;2024.All rights reserved.Powered by junevaRentz</p>
+      <p>www.junevaCarRentals.com</p>
+      <p>copyright &copy;2024.All rights reserved.</p>
     </div>
   )
 }
