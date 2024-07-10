98570d6c2bfe4a40970b11e7ec58f6f19bb805fd
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-10 21:20:06 +0300

    changes

---
+++ src/features/location/LocationApi.ts
@@ -0,0 +1,65 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export interface Tlocation{
+    location_id:number;
+    name:string;
+    address:string;
+    contact_phone:string;
+    created_at:string;
+    updated_at:string
+  }
+
+// Define the API slice
+export const LocationAPI = createApi({
+  reducerPath: 'locationAPI',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:8000',
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem('token');
+      console.log('Token:', token);
+      if (token) {
+        headers.set('Authorization', `${token}`);
+        
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ['locationBranches'],
+  endpoints: (builder) => ({
+    getLocation: builder.query<Tlocation[], void>({
+      query: () => 'locationBranches',
+      providesTags: ['locationBranches'],
+    }),
+    createLocation: builder.mutation<Tlocation, Partial<Tlocation>>({
+      query: (newLocationBranches) => ({
+        url: 'locationBranches',
+        method: 'POST',
+        body: newLocationBranches,
+      }),
+      invalidatesTags: ['locationBranches'],
+    }),
+    updateLocation: builder.mutation<Tlocation, Partial<Tlocation>>({
+      query: ({ location_id, ...rest }) => ({
+        url: `locationBranches/${location_id}`,
+        method: 'PUT',
+        body: rest,
+      }),
+      invalidatesTags: ['locationBranches'],
+    }),
+    deleteLocation: builder.mutation<{ success: boolean; location_id: number }, number>({
+      query: (location_id) => ({
+        url: `locationBranches/${location_id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['locationBranches'],
+    }),
+  }),
+});
+
+// Export the auto-generated hooks
+export const {
+  useGetLocationQuery,
+  useCreateLocationMutation,
+  useUpdateLocationMutation,
+  useDeleteLocationMutation,
+}:any = LocationAPI;
