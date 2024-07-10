56eca573d8a033d4bdc9754707af5e776486c0d0
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-10 21:13:24 +0300

    changes

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,44 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { loginApi } from '../features/login/LoginApi';
+import { registerApi } from '../features/register/RegisterApi';
+import { usersAPI } from '../features/users_management/UsersApi';
+import { LocationAPI } from '../features/location/LocationApi';
+import { customerAPI } from '../features/customer_support_tickets/CustomerApi';
+import { FleetAPI } from '../features/fleet_Management/FleetAPI';
+//auth persist config
+const persistConfig = {
+  key: 'auth',
+  storage,
+}
 
+//combine all reducers
+const rootReducer = combineReducers({
+  [loginApi.reducerPath]: loginApi.reducer,
+  [registerApi.reducerPath]:registerApi.reducer,
+  [usersAPI.reducerPath]: usersAPI.reducer,
+  [LocationAPI.reducerPath]:LocationAPI.reducer,
+  [customerAPI.reducerPath]:customerAPI.reducer,
+ [FleetAPI.reducerPath]:FleetAPI.reducer,
+});
 
-export const store= configureStore({
-    reducer:{
+//apply pesist Reducer to only counter reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+// store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck:{
+        ignoredActions:['persist/PERSIST','persist/REHYDRATE'],
+      },
+    }).concat(loginApi.middleware).concat(registerApi.middleware)
+    .concat(usersAPI.middleware).concat(LocationAPI.middleware).concat(customerAPI.middleware)
+    .concat(FleetAPI.middleware),
+});
 
-    }
-})
+export const persistedStore = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
