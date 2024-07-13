562d8330ebb9b040daecf430aa053bd5a40e05e5
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-13 08:22:20 -0500

    changes

--- src/dashboard/AdminDashboard.tsx
+++ src/dashboard/AdminDashboard.tsx
@@ -1,40 +1,42 @@
-import { FaHome } from 'react-icons/fa';
+import { RiLogoutCircleLine } from "react-icons/ri";
 import { Link, Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
 import { CiSettings } from "react-icons/ci";
-import { FaUserCircle } from "react-icons/fa";
-
+import { MdAdminPanelSettings } from "react-icons/md";
+import { Car } from 'lucide-react';
+import { TicketCheck } from 'lucide-react';
+import { MapPin } from 'lucide-react';
+import { ClipboardPlus } from 'lucide-react';
+import { UserRoundCog } from 'lucide-react';
+import { CarFront } from 'lucide-react';
+import { LayoutDashboard } from 'lucide-react';
 const AdminDashboard = () => {
   return (
     <>
-    <div className="bg-black h-16 flex items-center justify-between px-4">
-        <div className="flex items-center text-white">
-          <FaHome className="text-white mr-2" />
-          <button type='submit' className='py-5'><Link to='/' className='hover:border-b-2 hover:text-blue-400 text-white'>HOME</Link></button>
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-400 text-white hover:bg-primary/80 transition duration-500 rounded-md w-20 h-10 float-right m-2 "
-        >
-          Logout
+    <div className="bg-black h-16 flex items-center  px-4">
+        <button type="submit" className="bg-blue-400 text-white hover:bg-primary/80 transition duration-500 rounded-md w-20 h-10 ml-auto m-2 " ><div className="flex items-center text-white">
+          <RiLogoutCircleLine className='text-white'/>
+         <Link to='/'>Logout</Link> 
+         </div>
         </button>
+        
       </div> 
     <div className="flex">
       <div className="w-1/4 bg-gray-800 h-screen p-4 text-white">
       <div className="flex items-center justify-start gap-3 pb-3">
-          <FaUserCircle size={50} />
+          <MdAdminPanelSettings size={50} />
           <div>
             <h1 className="pt-2">Hello Admin</h1>
           </div>
         </div>
         <ul>
-          <li className="mb-2"><Link to="">Dashboard</Link></li>
-          <li className="mb-2"><Link to="vehicles">Manage Vehicles</Link></li>
-          <li className="mb-2"><Link to="users">Manage Users</Link></li>
-          <li className="mb-2"><Link to="reports">Reports</Link></li>
-          <li className="mb-2"><Link to="location">Location and Branches</Link></li>
-          <li className="mb-2"><Link to="customer-support">Customer support tickets</Link></li>
-          <li className="mb-2"><Link to="fleet">Fleet management</Link></li>
+          <li className="mb-2"><div className="flex items-center text-white"><LayoutDashboard className="text-white mr-2" /><Link to="">Dashboard</Link></div></li>
+          <li className="mb-2"><div className="flex items-center text-white"><CarFront className="text-white mr-2"  /><Link to="vehicles">Manage Vehicles</Link></div></li>
+          <li className="mb-2"><div className="flex items-center text-white"><UserRoundCog  className="text-white mr-2"  /><Link to="users">Manage Users</Link></div></li>
+          <li className="mb-2"><div className="flex items-center text-white"> <ClipboardPlus  className="text-white mr-2" /><Link to="reports">Reports</Link></div></li>
+          <li className="mb-2"><div className="flex items-center text-white"><MapPin  className="text-white mr-2" /><Link to="location">Location and Branches</Link></div></li>
+          <li className="mb-2"><div className="flex items-center text-white"><TicketCheck  className="text-white mr-2"/><Link to="customer-support">Customer support tickets</Link></div></li>
+          <li className="mb-2"><div className="flex items-center text-white"> <Car className="text-white mr-2"/><Link to="fleet">Fleet management</Link></div></li>
           <li className="mb-2"><div className="flex items-center text-white"><CiSettings  className="text-white mr-2"/><Link to="my-tickets">Settings</Link></div></li>
         </ul>
       </div>
