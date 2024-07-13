985c7c20c09334cdfa3236cf48c08efda58ea569
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-13 08:33:22 -0500

    added file

---
+++ src/features/customer_support_tickets/TicketApi.ts
@@ -0,0 +1,67 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export interface Tticket{
+    ticket_id:number;
+    user_id:number;
+    subject:string;
+    description:string;
+    status:string;
+    created_at:string;
+    updated_at:string
+  }
+
+// Define the API slice
+export const TicketAPI = createApi({
+  reducerPath: 'TicketAPI',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:8000',
+    prepareHeaders: (headers) => {
+      const userDetails = JSON.parse(localStorage.getItem('userDetails')||'{}');
+      const token=userDetails?.token;
+      console.log('Token:', token);
+      if (token) {
+        headers.set('Authorization', `${token}`);
+        
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ['customerSupportTickets'],
+  endpoints: (builder) => ({
+    getTicket: builder.query<Tticket[], void>({
+      query: () => 'customerSupportTickets',
+      providesTags: ['customerSupportTickets'],
+    }),
+    createTicket: builder.mutation<Tticket, Partial<Tticket>>({
+      query: (newCustomerSupportTickets) => ({
+        url: 'customerSupportTickets',
+        method: 'POST',
+        body: newCustomerSupportTickets,
+      }),
+      invalidatesTags: ['customerSupportTickets'],
+    }),
+    updateTicket: builder.mutation<Tticket, Partial<Tticket>>({
+      query: ({ ticket_id, ...rest }) => ({
+        url: `customerSupportTickets/${ticket_id}`,
+        method: 'PUT',
+        body: rest,
+      }),
+      invalidatesTags: ['customerSupportTickets'],
+    }),
+    deleteCustomerSupportTickets: builder.mutation<{ success: boolean; ticket_id: number }, number>({
+      query: (ticket_id) => ({
+        url: `customerSupportTickets/${ticket_id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['customerSupportTickets'],
+    }),
+  }),
+});
+
+// Export the auto-generated hooks
+export const {
+  useGetTicketQuery,
+  useCreateTicketMutation,
+  useUpdateTicketMutation,
+  useDeleteCustomerSupportTicketsMutation,
+}:any = TicketAPI;
