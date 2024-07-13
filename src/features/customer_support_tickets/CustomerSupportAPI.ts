281e682ba475aa7cf30bdc210bffd03d08034070
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-13 08:30:35 -0500

    added file

---
+++ src/features/customer_support_tickets/CustomerSupportAPI.ts
@@ -0,0 +1,75 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export interface TcustomerSupport{
+    ticket_id:number;
+    user_id:number;
+    subject:string;
+    description:string;
+    status:string;
+    created_at:string;
+    updated_at:string;
+ user:{
+     address:string;
+    contact_phone:string;
+      created_at:string;
+      email:string;
+      full_name:string;
+      updated_at:string;
+      }
+  }
+
+// Define the API slice
+export const customerSupportAPI = createApi({
+  reducerPath: 'customerSupportAPI',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:8000',
+    prepareHeaders: (headers) => {
+      const userDetails = JSON.parse(localStorage.getItem('userDetails')||'{}');
+      const token=userDetails?.token;
+      console.log('Token:', token);
+      if (token) {
+        headers.set('Authorization', `${token}`);
+        
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ['customerSupportTicketsData'],
+  endpoints: (builder) => ({
+    getCustomerSupport: builder.query<TcustomerSupport[], void>({
+      query: () => 'customerSupportTicketsData',
+      providesTags: ['customerSupportTicketsData'],
+    }),
+    createCustomerSupport: builder.mutation<TcustomerSupport, Partial<TcustomerSupport>>({
+      query: (newCustomerSupportTicketsData) => ({
+        url: 'customerSupportTicketsData',
+        method: 'POST',
+        body: newCustomerSupportTicketsData,
+      }),
+      invalidatesTags: ['customerSupportTicketsData'],
+    }),
+    updateCustomerSupport: builder.mutation<TcustomerSupport, Partial<TcustomerSupport>>({
+      query: ({ ticket_id, ...rest }) => ({
+        url: `customerSupportTicketsData/${ticket_id}`,
+        method: 'PUT',
+        body: rest,
+      }),
+      invalidatesTags: ['customerSupportTicketsData'],
+    }),
+    deleteCustomerSupport: builder.mutation<{ success: boolean; ticket_id: number }, number>({
+      query: (ticket_id) => ({
+        url: `customerSupportTicketsData/${ticket_id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['customerSupportTicketsData'],
+    }),
+  }),
+});
+
+// Export the auto-generated hooks
+export const {
+  useGetCustomerSupportQuery,
+  useCreateCustomerSupportMutation,
+  useUpdateCustomerSupportMutation,
+  useDeleteCustomerSupportMutation,
+}:any = customerSupportAPI;
