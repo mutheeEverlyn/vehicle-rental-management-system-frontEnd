304f7d80350619b0004565b983ab95d81304919b
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-13 08:23:12 -0500

    changes

--- src/dashboard/UserDashboard.tsx
+++ src/dashboard/UserDashboard.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import { FaHome } from 'react-icons/fa';
+import { RiLogoutCircleLine } from "react-icons/ri";
 import Footer from '../components/Footer';
 import { FaUserCircle } from "react-icons/fa";
-
+import { LayoutDashboard } from 'lucide-react';
+import { Car } from 'lucide-react';
+import { CarFront } from 'lucide-react';
+import { TicketCheck } from 'lucide-react';
+import { TicketPlus } from 'lucide-react';
 const UserDashboard: React.FC = () => {
   return (
     <>
-      <div className="bg-black h-16 flex items-center justify-between px-4">
-        <div className="flex items-center text-white">
-          <FaHome className="text-white mr-2" />
-          <button type='submit' className='py-5'><Link to='/' className='hover:border-b-2 hover:text-blue-400 text-white'>HOME</Link></button>
-        </div>
-        <button
-          type="submit"
-          className="bg-blue-400 text-white hover:bg-primary/80 transition duration-500 rounded-md w-20 h-10 float-right m-2"
-        >
-          Logout
+    <div className="bg-black h-16 flex items-center  px-4">
+        <button type="submit" className="bg-blue-400 text-white hover:bg-primary/80 transition duration-500 rounded-md w-20 h-10 float-right ml-auto m-2 " ><div className="flex items-center text-white">
+          <RiLogoutCircleLine className='text-white'/>
+         <Link to='/'>Logout</Link> 
+         </div>
         </button>
-      </div>
+        
+      </div> 
       <div className="flex">
         <div className="w-1/4 bg-gray-800 h-screen p-4 text-white">
         <div className="flex items-center pb-3 justify-start gap-3">
@@ -28,11 +28,12 @@ const UserDashboard: React.FC = () => {
           </div>
         </div>
           <ul>
-            <li className="mb-2"><Link to="">Dashboard</Link></li>
-            <li className="mb-2"><Link to="book-vehicle">Book a Vehicle</Link></li>
-            <li className="mb-2"><Link to="booked-vehicles">Booked Vehicles</Link></li>
-            <li className="mb-2"><Link to="my-tickets">My Tickets</Link></li>
-            <li className="mb-2"><Link to="new-ticket">New Ticket</Link></li>
+          <li className="mb-2"><div className="flex items-center text-white"><LayoutDashboard className="text-white mr-2" /><Link to="">Dashboard</Link></div></li>
+            <li className="mb-2"><div className="flex items-center text-white"> <Car className="text-white mr-2"/><Link to="book-vehicle">Book a Vehicle</Link></div></li>
+            <li className="mb-2"><div className="flex items-center text-white"><CarFront className="text-white mr-2"  /><Link to="bookings">My bookings</Link></div></li>
+            <li className="mb-2"><div className="flex items-center text-white"><TicketCheck  className="text-white mr-2"/><Link to="my-tickets">My Tickets</Link></div></li>
+            <li className="mb-2"><div className="flex items-center text-white"><TicketPlus className="text-white mr-2" /><Link to="new-ticket">New Ticket</Link></div></li>
+            <li className="mb-2"><div className="flex items-center text-white"><TicketPlus className="text-white mr-2" /><Link to="profile">My profile</Link></div></li>
           </ul>
         </div>
         <div className="w-3/4 p-4">
