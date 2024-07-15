6e0b44dab1a4ea03bf8936b456313a768b2398a0
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-15 16:54:42 +0300

    added new file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,25 +11,25 @@ const Navbar = () => {
     setShowMenu(!showMenu);
   };
   return (
-    <div className='flex gap-5 bg-black text-white  p-5 w-full  relative z-10 shadow-md duration-300'>
-      <div className='w-40 md:w-56'><img src={logo} width={300} alt="logo"/></div>
-      <nav className='hidden  md:flex md:justify-between md:gap-10  ml-auto '>
-      <ul className='flex   gap-5 items-center '>
+    <div className='flex gap-10 bg-black text-white  p-5 w-full  relative  shadow-md duration-300 md:items-center justify-between  md:gap-0 '>
+      <div className='w-40 lg:w-60 md:w-32 '><img src={logo} width={300} alt="logo"/></div>
+      <nav className='hidden  md:flex md:justify-between md:gap-1   '>
+      <ul className='flex   gap-5 items-center md:gap-3 '>
         <li><button><Link to='/' className='hover:border-b-2 hover:text-blue-400'>HOME</Link></button></li>
         <li><button><Link to='/about'  className='hover:border-b-2 hover:text-blue-400'>ABOUT US</Link></button></li>
-        <li><button><Link to='/listing'  className='hover:border-b-2 hover:text-blue-400'>CAR LISTING</Link></button></li>
-        <li><button><Link to='/services'  className='hover:border-b-2 hover:text-blue-400'>SERVICES</Link></button></li>
-        <li><button><Link to='/contact'  className='hover:border-b-2 hover:text-blue-400'>CONTACT US</Link></button></li>
+        <li><button><Link to='/carList'  className='hover:border-b-2 hover:text-blue-400'>CAR LISTING</Link></button></li>
+        <li><button><Link to='/moreServices'  className='hover:border-b-2 hover:text-blue-400'>SERVICES</Link></button></li>
+        <li><button><Link to='/contactUs'  className='hover:border-b-2 hover:text-blue-400'>CONTACT US</Link></button></li>
       </ul>
-      <div className='flex gap-5 items-center '>
+      <div className='flex gap-5 items-center md:gap-2 lg:ml-40'>
       <button className=' bg-blue-400  hover:bg-primary/80 transition duration-500 rounded-md w-20 h-10 my-10'><Link to='/login'> LOGIN</Link></button>
       <button className='bg-blue-400  hover:bg-primary/80 transition duration-500 rounded-md w-20 h-10 my-10 '><Link to='/register'>REGISTER</Link></button>
       </div>
       </nav>
       {showMenu?(
-       <button onClick={toggleMenu} className='cursor-pointer transition-all ml-auto'><img src={close} alt='close icon' className='bg-transparent  m-auto md:hidden h-10 '/></button>
+       <button onClick={toggleMenu} className='cursor-pointer transition-all '><img src={close} alt='close icon' className='bg-transparent  m-auto md:hidden h-10 '/></button>
       ):(
-        <button onClick={toggleMenu} className='ml-auto cursor-pointer transition-all' >< img src={hamburger} alt='icon humburger' className='bg-transparent  m-auto md:hidden h-10 '/></button>
+        <button onClick={toggleMenu} className=' cursor-pointer transition-all' >< img src={hamburger} alt='icon humburger' className='bg-transparent  m-auto md:hidden h-10 '/></button>
       )}
       <MobileMenu showItems={showMenu}/>
     </div>
