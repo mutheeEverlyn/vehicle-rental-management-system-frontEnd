94d7583c9b6e12f9a5890a779339c86a3390ed00
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-15 16:53:21 +0300

    added new file

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,26 @@
-
+import { FaFacebook,FaInstagram,FaLinkedin, FaMobileAlt,} from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div className="text-sm sm:text-lg left-0 w-full flex p-10 max-w-full gap-5 justify-center bg-black text-white bottom-0">
-      
+    <div className="text-sm  bottom-0 left-0 w-full flex  md:text-lg py-5 items-center max-w-full gap-5 justify-center bg-black text-white  mt-auto">
+      <div className="flex items-center  gap-3">
+            <div className="flex items-center gap-3 mt-3">
+              <FaMobileAlt />
+              <p>+254 73456789</p>
+            </div>
+            <div className="flex items-center gap-3 mt-6">
+              <a href="#">
+                <FaInstagram className="text-3xl hover:text-primary duration-300" />
+              </a>
+              <a href="#">
+                <FaFacebook className="text-3xl hover:text-primary duration-300" />
+              </a>
+              <a href="#">
+                <FaLinkedin className="text-3xl hover:text-primary duration-300" />
+              </a>
+            </div>
+          </div>
+    
       <p>www.junevaCarRentals.com</p>
       <p>copyright &copy;2024.All rights reserved.</p>
     </div>
