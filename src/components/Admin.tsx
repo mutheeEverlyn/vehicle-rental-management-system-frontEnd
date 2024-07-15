cea81c471ca38b1969b1d1a6cb46ffbf444483dc
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-15 16:50:46 +0300

    added new file

---
+++ src/components/Admin.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import TicketStatusChart from './TicketStatusChart';
+import { useGetCustomerSupportQuery } from '../features/customer_support_tickets/CustomerSupportAPI';
+
+const Admin: React.FC = () => {
+  const { data, error, isLoading, isError } = useGetCustomerSupportQuery();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error?.message || 'An error occurred'}</div>;
+  }
+
+  return (
+    <div>
+      <TicketStatusChart data={data || []} />
+    </div>
+  );
+};
+
+export default Admin;
