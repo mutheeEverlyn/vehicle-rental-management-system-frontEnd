90ab73cf08bb81e7bafdf3d02648b2a9a6b72846
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-16 16:34:54 +0300

    changes

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom";
 const Contact = () => {
   return (
     <>
       <div  className=" py-14 ">
         <div className="w-full max-w-full ">
-          <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 bg-gray-800 py-8 px-6">
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 bg-blue-400 py-8 px-6">
             <div className="col-span-2 space-y-3">
               <h1 className="text-4xl sm:text-5xl font-bold text-white">
                 Let's collaborate on your upcoming vehicle rental venture to have an amaizing momment
@@ -13,8 +14,8 @@ const Contact = () => {
               </p>
             </div>
             <div className="sm:grid sm:place-items-center">
-              <button className="inline-block font-semibold  hover:bg-primary/80 transition duration-500 py-2 px-6 bg-blue-400 text-white tracking-widest uppercase rounded-md" >
-                Contact Us
+              <button className="inline-block font-semibold  hover:bg-primary/80 transition duration-500 py-2 px-6 bg-blue-500 text-white tracking-widest uppercase rounded-md" >
+               <Link to='/contactUs'>Contact Us</Link> 
               </button>
             </div>
           </div>
