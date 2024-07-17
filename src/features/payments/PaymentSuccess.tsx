3bd3e238093083bcc777a8f1552e043dd658d365
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-17 12:20:46 +0300

    added new file

---
+++ src/features/payments/PaymentSuccess.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom"
+const Payments = () => {
+  return (
+    <div className="card bg-neutral m-auto text-neutral-content w-96">
+    <div className="card-body items-center text-center">
+      <h2 className="card-title">Thank you!</h2>
+      <p>payment made successfully</p>
+      <div className="card-actions justify-end">
+        <button className="btn btn-primary"><Link to='/'>Home</Link></button>
+        <button className="btn btn-ghost"><Link to='/userDashboard/bookings'>Go Back</Link></button>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Payments
