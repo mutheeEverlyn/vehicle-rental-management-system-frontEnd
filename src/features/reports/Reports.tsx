aa80aa2032b491ce99d48a9d662742850ce59c59
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-17 12:23:57 +0300

    added new file

---
+++ src/features/reports/Reports.tsx
@@ -0,0 +1,12 @@
+import BookingSummary from "../../components/BookingSummary"
+import RevenueReport from "../../components/RevenueReport"
+const Reports = () => {
+  return (
+    <div>
+      <BookingSummary/>
+      <RevenueReport/>
+    </div>
+  )
+}
+
+export default Reports
