0d7a9bd8247159d4f1e79f8a965f2e64b6f6091f
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-22 07:36:57 +0300

    changes

--- src/features/login/LoginApi.ts
+++ src/features/login/LoginApi.ts
@@ -1,30 +1,36 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export interface logInUser {
-    email: string;
-    password: string;
+export interface LogInUser {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  user: {
+    user_id: number;
+    role: string;
+  };
+  token: string;
 }
 
 export const loginApi = createApi({
-    reducerPath: 'loginAPI',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000/users' }),
-    endpoints: (builder) => ({
-        loginUser: builder.mutation<logInUser, Partial<logInUser>>({
-            query: (user) => ({
-                url: 'login',
-                method: 'POST',
-                body: user,
-            }),
-        }),
-        logout: builder.mutation<logInUser, Partial<logInUser>>({
-            query: (user) => ({
-                url: 'logout',
-                method: 'POST',
-                body: user,
-            }),
-        })
-
+  reducerPath: 'loginAPI',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:8000' }),
+  endpoints: (builder) => ({
+    loginUser: builder.mutation<LoginResponse, Partial<LogInUser>>({
+      query: (user) => ({
+        url: 'login',
+        method: 'POST',
+        body: user,
+      }),
     }),
+    logout: builder.mutation<void, void>({
+      query: () => ({
+        url: 'logout',
+        method: 'POST',
+      }),
+    }),
+  }),
 });
 
-export const { useLoginUser, useLogout } = loginApi;
+export const { useLoginUserMutation, useLogoutMutation }:any = loginApi;
