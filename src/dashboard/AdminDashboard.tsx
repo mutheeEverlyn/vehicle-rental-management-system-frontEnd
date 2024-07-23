93c5d3cdffaa99aa71afd4117db27f29e1b113dc
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-23 15:15:52 +0300

    Navlink implementation in dashboards

--- src/dashboard/AdminDashboard.tsx
+++ src/dashboard/AdminDashboard.tsx
@@ -1,25 +1,21 @@
 import { RiLogoutCircleLine } from "react-icons/ri";
-import hamburger from '../assets/icon-hamburger.svg'
-import close from '../assets/icon-close.svg'
-import { useState } from 'react'
+import hamburger from '../assets/icon-hamburger.svg';
+import close from '../assets/icon-close.svg';
+import { useState } from 'react';
 import AdminPhoneDashboard from "./AdminPhoneDashboard";
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet,Link } from 'react-router-dom';
 import Footer from '../components/Footer';
-import { CircleDollarSign } from 'lucide-react';
+import { CircleDollarSign, LayoutDashboard, CarFront, UserRoundCog, MapPin, TicketCheck, Car } from 'lucide-react';
 import { MdAdminPanelSettings } from "react-icons/md";
-import { Car } from 'lucide-react';
-import { TicketCheck } from 'lucide-react';
-import { MapPin } from 'lucide-react';
-import { UserRoundCog } from 'lucide-react';
-import { CarFront } from 'lucide-react';
-import { LayoutDashboard } from 'lucide-react';
 import { useGetUserByIdQuery } from '../features/users_management/UsersApi';
+
 const AdminDashboard = () => {
   const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
+
   const userDetails = localStorage.getItem('userDetails');
   if (!userDetails) {
     return <div>Error: No user is logged in.</div>;
@@ -30,52 +26,122 @@ const AdminDashboard = () => {
   const { data, error, isLoading } = useGetUserByIdQuery(user_id);
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.toString()}</div>;
-  const handleLogout=()=>{
-    localStorage.removeItem('userDetails');
 
-  }
+  const handleLogout = () => {
+    localStorage.removeItem('userDetails');
+  };
+
   return (
     <>
-    <div className="bg-black h-16 flex items-center justify-between px-4">
-        <button onClick={handleLogout} type="submit" className="bg-blue-400 text-white hover:bg-primary/80 transition duration-500 rounded-md w-20 h-10 md:ml-auto m-2 " ><div className="flex items-center text-white">
-          <RiLogoutCircleLine className='text-white'/>
-         <Link to='/'>Logout</Link> 
-         </div>
-        </button>
-        {showMenu?(
-       <button onClick={toggleMenu} className='cursor-pointer transition-all '><img src={close} alt='close icon' className='bg-transparent  m-auto md:hidden h-10 '/></button>
-      ):(
-        <button onClick={toggleMenu} className=' cursor-pointer transition-all' >< img src={hamburger} alt='icon humburger' className='bg-transparent  m-auto md:hidden h-10 '/></button>
-      )}
-      <AdminPhoneDashboard showItems={showMenu}/>
-      </div> 
-    <div className="flex min-h-screen">
-      <div className="w-full md:w-1/5 bg-gray-800  p-4 text-white md:block hidden">
-      <div className="flex items-center justify-start gap-3 pb-3">
-          <MdAdminPanelSettings size={50} />
-          <div>
-            <h1 className="pt-2">Welcome {data.full_name}</h1>
+      <div className="bg-black h-16 flex items-center justify-between px-4">
+        <button
+          onClick={handleLogout}
+          type="submit"
+          className="bg-blue-400 text-white hover:bg-primary/80 transition duration-500 rounded-md w-20 h-10 md:ml-auto m-2"
+        >
+          <div className="flex items-center text-white">
+            <RiLogoutCircleLine className='text-white' />
+            <Link to='/'>Logout</Link>
           </div>
+        </button>
+        {showMenu ? (
+          <button onClick={toggleMenu} className='cursor-pointer transition-all'>
+            <img src={close} alt='close icon' className='bg-transparent m-auto md:hidden h-10' />
+          </button>
+        ) : (
+          <button onClick={toggleMenu} className='cursor-pointer transition-all'>
+            <img src={hamburger} alt='hamburger icon' className='bg-transparent m-auto md:hidden h-10' />
+          </button>
+        )}
+        <AdminPhoneDashboard showItems={showMenu} />
+      </div>
+      <div className="flex min-h-screen">
+        <div className="w-full md:w-1/5 bg-gray-800 p-4 text-white md:block hidden">
+          <div className="flex items-center justify-start gap-3 pb-3">
+            <MdAdminPanelSettings size={50} />
+            <div>
+              <h1 className="pt-2">Welcome {data.full_name}</h1>
+            </div>
+          </div>
+          <ul>
+            <li className="mb-2">
+              <NavLink to="" className={({ isActive }) => `flex items-center text-white ${isActive ? 'text-blue-500' : ''}`}>
+                <LayoutDashboard className="text-white mr-2" />
+                Dashboard
+              </NavLink>
+            </li>
+            <li className="mb-2">
+              <NavLink
+                to="vehicles"
+                className={({ isActive }) => `flex items-center text-white ${isActive ? 'text-blue-500' : ''}`}
+              >
+                <CarFront className="text-white mr-2" />
+                Manage Vehicles
+              </NavLink>
+            </li>
+            <li className="mb-2">
+              <NavLink
+                to="book-vehicle"
+                className={({ isActive }) => `flex items-center text-white ${isActive ? 'text-blue-500' : ''}`}
+              >
+                <CarFront className="text-white mr-2" />
+                Vehicles List
+              </NavLink>
+            </li>
+            <li className="mb-2">
+              <NavLink
+                to="users"
+                className={({ isActive }) => `flex items-center text-white ${isActive ? 'text-blue-500' : ''}`}
+              >
+                <UserRoundCog className="text-white mr-2" />
+                Manage Users
+              </NavLink>
+            </li>
+            <li className="mb-2">
+              <NavLink
+                to="location"
+                className={({ isActive }) => `flex items-center text-white ${isActive ? 'text-blue-500' : ''}`}
+              >
+                <MapPin className="text-white mr-2" />
+                Location and Branches
+              </NavLink>
+            </li>
+            <li className="mb-2">
+              <NavLink
+                to="customer-support"
+                className={({ isActive }) => `flex items-center text-white ${isActive ? 'text-blue-500' : ''}`}
+              >
+                <TicketCheck className="text-white mr-2" />
+                Customer support tickets
+              </NavLink>
+            </li>
+            <li className="mb-2">
+              <NavLink
+                to="fleet"
+                className={({ isActive }) => `flex items-center text-white ${isActive ? 'text-blue-500' : ''}`}
+              >
+                <Car className="text-white mr-2" />
+                Fleet management
+              </NavLink>
+            </li>
+            <li className="mb-2">
+              <NavLink
+                to="paymentsInfo"
+                className={({ isActive }) => `flex items-center text-white ${isActive ? 'text-blue-500' : ''}`}
+              >
+                <CircleDollarSign className="text-white mr-2" />
+                Payments
+              </NavLink>
+            </li>
+          </ul>
         </div>
-        <ul>
-          <li className="mb-2"><div className="flex items-center text-white"><LayoutDashboard className="text-white mr-2" /><Link to="">Dashboard</Link></div></li>
-          <li className="mb-2"><div className="flex items-center text-white"><CarFront className="text-white mr-2"  /><Link to="vehicles">Manage Vehicles</Link></div></li>
-          <li className="mb-2"><div className="flex items-center text-white"><CarFront className="text-white mr-2"  /><Link to="book-vehicle">Vehicles List</Link></div></li>
-          <li className="mb-2"><div className="flex items-center text-white"><UserRoundCog  className="text-white mr-2"  /><Link to="users">Manage Users</Link></div></li>
-          <li className="mb-2"><div className="flex items-center text-white"><MapPin  className="text-white mr-2" /><Link to="location">Location and Branches</Link></div></li>
-          <li className="mb-2"><div className="flex items-center text-white"><TicketCheck  className="text-white mr-2"/><Link to="customer-support">Customer support tickets</Link></div></li>
-          <li className="mb-2"><div className="flex items-center text-white"> <Car className="text-white mr-2"/><Link to="fleet">Fleet management</Link></div></li>
-          <li className="mb-2"><div className="flex items-center text-white"><CircleDollarSign className="text-white mr-2"/><Link to="paymentsInfo">payments</Link></div></li>
-        </ul>
+        <div className="w-full md:w-3/4 p-4">
+          <Outlet /> {/* This renders the matched child route */}
+          <Footer />
+        </div>
       </div>
-      <div className="w-full md:w-3/4 p-4">
-        <Outlet /> {/* This renders the matched child route */}
-        <Footer />
-      </div>
-    
-    </div>
     </>
   );
 };
 
-export default AdminDashboard
+export default AdminDashboard;
--- src/dashboard/UserDashboard.tsx
+++ src/dashboard/UserDashboard.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import hamburger from '../assets/icon-hamburger.svg'
-import close from '../assets/icon-close.svg'
-import { useState } from 'react'
+import React, { useState } from 'react';
+import hamburger from '../assets/icon-hamburger.svg';
+import close from '../assets/icon-close.svg';
 import UserPhoneDashboard from './UserPhoneDashboard';
-import { Link, Outlet } from 'react-router-dom';
+import { NavLink, Outlet,Link } from 'react-router-dom';
 import { RiLogoutCircleLine } from "react-icons/ri";
 import Footer from '../components/Footer';
 import { FaUserCircle } from "react-icons/fa";
@@ -12,9 +11,11 @@ import { useGetUserByIdQuery } from '../features/users_management/UsersApi';
 
 const UserDashboard: React.FC = () => {
   const [showMenu, setShowMenu] = useState(false);
+
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
+
   const userDetails = localStorage.getItem('userDetails');
   if (!userDetails) {
     return <div>Error: No user is logged in.</div>;
@@ -25,39 +26,100 @@ const UserDashboard: React.FC = () => {
   const { data, error, isLoading } = useGetUserByIdQuery(user_id);
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.toString()}</div>;
-  const handleLogout=()=>{
-    localStorage.removeItem('userDetails');
 
-  }
+  const handleLogout = () => {
+    localStorage.removeItem('userDetails');
+  };
+
   return (
     <>
       <div className="bg-black h-16 flex items-center justify-between px-4">
-        <button onClick={handleLogout} type="submit" className="bg-blue-400 text-white hover:bg-primary/80 transition duration-500 rounded-md w-20 h-10 float-right md:ml-auto m-2">
-          <div className="flex items-center text-white"><RiLogoutCircleLine className='text-white' /><Link to='/'>Logout</Link> </div>
+        <button onClick={handleLogout} type="submit" className="bg-blue-400 text-white hover:bg-primary/80 transition duration-500 rounded-md w-20 h-10 float-right md:ml-auto m-2" >
+          <div className="flex items-center text-white">
+            <RiLogoutCircleLine className='text-white' />
+            <Link to='/'>Logout</Link>
+          </div>
         </button>
-        {showMenu?(
-       <button onClick={toggleMenu} className='cursor-pointer transition-all '><img src={close} alt='close icon' className='bg-transparent  m-auto md:hidden h-10 '/></button>
-      ):(
-        <button onClick={toggleMenu} className=' cursor-pointer transition-all' >< img src={hamburger} alt='icon humburger' className='bg-transparent  m-auto md:hidden h-10 '/></button>
-      )}
-      <UserPhoneDashboard showItems={showMenu}/>
-      
+        {showMenu ? (
+          <button onClick={toggleMenu} className='cursor-pointer transition-all'>
+            <img src={close} alt='close icon' className='bg-transparent m-auto md:hidden h-10' />
+          </button>
+        ) : (
+          <button onClick={toggleMenu} className='cursor-pointer transition-all'>
+            <img src={hamburger} alt='hamburger icon' className='bg-transparent m-auto md:hidden h-10' />
+          </button>
+        )}
+        <UserPhoneDashboard showItems={showMenu} />
       </div>
       <div className="flex min-h-screen">
         <div className="md:w-1/5 w-full bg-gray-800 p-4 text-white hidden md:block">
-          <div className="flex items-center pb-3 justify-start gap-3"> <FaUserCircle size={50} />
-            <div><h1 className="pt-2 ">Welcome {data.full_name}</h1> </div>
+          <div className="flex items-center pb-3 justify-start gap-3">
+            <FaUserCircle size={50} />
+            <div>
+              <h1 className="pt-2">Welcome {data.full_name}</h1>
+            </div>
           </div>
           <ul>
-            <li className="mb-2"><div className="flex items-center text-white"> <LayoutDashboard className="text-white mr-2" /><Link to="">Dashboard</Link> </div></li>
-            <li className="mb-2"><div className="flex items-center text-white"><Car className="text-white mr-2" /><Link to="book-vehicle">Book a Vehicle</Link></div> </li>
-            <li className="mb-2"><div className="flex items-center text-white"><CarFront className="text-white mr-2" /><Link to="bookings">My bookings</Link></div> </li>
-           <li className="mb-2"><div className="flex items-center text-white"><TicketCheck className="text-white mr-2" /> <Link to="my-tickets">My Tickets</Link></div></li>
-            <li className="mb-2"><div className="flex items-center text-white"><TicketPlus className="text-white mr-2" /><Link to="new-ticket">New Ticket</Link> </div></li>
-            <li className="mb-2"> <div className="flex items-center text-white"><FaUserCircle size={30} className="text-white mr-2" /> <Link to="profile">My profile</Link> </div></li>
+            <li className="mb-2">
+              <NavLink
+                to=""
+                className={({ isActive }) => `flex items-center text-white ${isActive ? 'text-blue-500' : ''}`}
+              >
+                <LayoutDashboard className="text-white mr-2" />
+                Dashboard
+              </NavLink>
+            </li>
+            <li className="mb-2">
+              <NavLink
+                to="book-vehicle"
+                className={({ isActive }) => `flex items-center text-white ${isActive ? 'text-blue-500' : ''}`}
+              >
+                <Car className="text-white mr-2" />
+                Book a Vehicle
+              </NavLink>
+            </li>
+            <li className="mb-2">
+              <NavLink
+                to="bookings"
+                className={({ isActive }) => `flex items-center text-white ${isActive ? 'text-blue-500' : ''}`}
+              >
+                <CarFront className="text-white mr-2" />
+                My Bookings
+              </NavLink>
+            </li>
+            <li className="mb-2">
+              <NavLink
+                to="my-tickets"
+                className={({ isActive }) => `flex items-center text-white ${isActive ? 'text-blue-500' : ''}`}
+              >
+                <TicketCheck className="text-white mr-2" />
+                My Tickets
+              </NavLink>
+            </li>
+            <li className="mb-2">
+              <NavLink
+                to="new-ticket"
+                className={({ isActive }) => `flex items-center text-white ${isActive ? 'text-blue-500' : ''}`}
+              >
+                <TicketPlus className="text-white mr-2" />
+                New Ticket
+              </NavLink>
+            </li>
+            <li className="mb-2">
+              <NavLink
+                to="profile"
+                className={({ isActive }) => `flex items-center text-white ${isActive ? 'text-blue-500' : ''}`}
+              >
+                <FaUserCircle size={30} className="text-white mr-2" />
+                My Profile
+              </NavLink>
+            </li>
           </ul>
         </div>
-        <div className="w-full md:w-3/4 p-4"><Outlet context={{ user_id }} /> {/* Pass user_id via context */}<Footer /> </div>
+        <div className="w-full md:w-3/4 p-4">
+          <Outlet context={{ user_id }} /> {/* Pass user_id via context */}
+          <Footer />
+        </div>
       </div>
     </>
   );
