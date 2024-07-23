56f2c459ebef42350e02c727cfc3ae864dd0c6a6
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-23 12:38:50 +0300

    vite changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Ensure this is 'dist'
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          // Add more manual chunks as necessary
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust this limit as necessary
+  },
+});
