89dcaaeeb40c9cbb0b512077931c64ca73b7f12d
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-24 09:13:32 +0300

    added a file

---
+++ src/components/ContactSuccess.tsx
@@ -0,0 +1,16 @@
+const ContactSuccess = () => {
+  return (
+      <div className="card bg-primary text-primary-content w-96">
+  <div className="card-body">
+    <h2 className="card-title">Thank you for contacting us!</h2>
+    <p>We received your feedback and will repond to it.You are welcome to book your dream car with us</p>
+    <div className="card-actions justify-end">
+      <button className="btn">Home</button>
+    </div>
+  </div>
+</div>
+    
+  )
+}
+
+export default ContactSuccess
