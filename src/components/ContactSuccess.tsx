4e786323ef7f1f3114a914f6a2206695b3f08665
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-24 11:41:26 +0300

    changes

--- src/components/ContactSuccess.tsx
+++ src/components/ContactSuccess.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 const ContactSuccess = () => {
   return (
     <div className="card bg-blue-400 m-auto text-neutral-content w-96">
@@ -5,7 +6,7 @@ const ContactSuccess = () => {
     <h2 className="card-title">Thank you for contacting us!</h2>
     <p>We received your feedback and will repond to it.You are welcome to book your dream car with us</p>
     <div className="card-actions justify-end">
-      <button className="btn">Home</button>
+      <button className="btn"><Link to='/'></Link>Home</button>
     </div>
   </div>
 </div>
