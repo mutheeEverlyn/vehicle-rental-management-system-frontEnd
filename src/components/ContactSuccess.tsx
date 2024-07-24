2dcca5e1bc4511a1ce1b7c659d1ce6de3c57eb08
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2024-07-24 11:45:28 +0300

    link to home

--- src/components/ContactSuccess.tsx
+++ src/components/ContactSuccess.tsx
@@ -6,7 +6,7 @@ const ContactSuccess = () => {
     <h2 className="card-title">Thank you for contacting us!</h2>
     <p>We received your feedback and will repond to it.You are welcome to book your dream car with us</p>
     <div className="card-actions justify-end">
-      <button className="btn"><Link to='/'></Link>Home</button>
+      <button className="btn"><Link to='/'>Home</Link></button>
     </div>
   </div>
 </div>
